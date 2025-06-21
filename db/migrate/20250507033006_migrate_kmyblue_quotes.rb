2827a2c63106479e31a4aed8987235ed7af18a5a
KMY <tt@kmycode.net>
2025-06-21 14:50:06 +0900

    insert bulkに変更

--- db/migrate/20250507033006_migrate_kmyblue_quotes.rb
+++ db/migrate/20250507033006_migrate_kmyblue_quotes.rb
@@ -7,13 +7,14 @@ class MigrateKmyblueQuotes < ActiveRecord::Migration[8.0]
   def up
     Status.where.not(quote_of_id: nil).select(:id, :quote_of_id, :account_id, :created_at).in_batches do |owner_statuses|
       quoted_statuses = Status.where(id: owner_statuses.pluck(:quote_of_id)).select(:id, :account_id)
+      quotes = []
 
       ActiveRecord::Base.transaction do
         owner_statuses.each do |owner_status|
           quoted_status = quoted_statuses.detect { |status| status.id == owner_status.quote_of_id }
           next unless quoted_status
 
-          Quote.create!(
+          quotes << {
             id: owner_status.id,
             status_id: owner_status.id,
             quoted_status_id: owner_status.quote_of_id,
@@ -21,22 +22,27 @@ class MigrateKmyblueQuotes < ActiveRecord::Migration[8.0]
             account_id: owner_status.account_id,
             quoted_account_id: quoted_status.account_id,
             created_at: owner_status.created_at,
-            updated_at: owner_status.created_at
-          )
+            updated_at: owner_status.created_at,
+          }
         end
       end
+
+      Quote.insert_all(quotes)
     end
   end
 
   def down
-    Quote.select(:quoted_status_id, :status_id).in_batches do |quotes|
+    Quote.select(:id, :quoted_status_id, :status_id).in_batches do |quotes|
       statuses = Status.where(id: quotes.pluck(:status_id))
+
       quotes.each do |quote|
         status = statuses.detect { |s| s.id == quote.status_id }
         next unless status
 
         status.update!(quote_of_id: quote.quoted_status_id)
       end
+
+      quotes.delete_all
     end
   end
 end
