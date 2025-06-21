d1f2dfd62e332c03af7b90c8948576dcba018191
KMY（雪あすか） <tt@kmycode.net>
2025-06-21 10:34:24 +0900

    Update 20250507033006_migrate_kmyblue_quotes.rb

--- db/migrate/20250507033006_migrate_kmyblue_quotes.rb
+++ db/migrate/20250507033006_migrate_kmyblue_quotes.rb
@@ -5,25 +5,23 @@ class MigrateKmyblueQuotes < ActiveRecord::Migration[8.0]
   class Quote < ApplicationRecord; end
 
   def up
-    ActiveRecord::Base.transaction do
-      Status.where.not(quote_of_id: nil).select(:id, :quote_of_id, :account_id).in_batches do |owner_statuses|
-        quoted_statuses = Status.where(id: owner_statuses.pluck(:quote_of_id)).select(:id, :account_id)
+    Status.where.not(quote_of_id: nil).select(:id, :quote_of_id, :account_id).in_batches do |owner_statuses|
+      quoted_statuses = Status.where(id: owner_statuses.pluck(:quote_of_id)).select(:id, :account_id)
 
-        owner_statuses.each do |owner_status|
-          quoted_status = quoted_statuses.detect { |status| status.id == owner_status.quote_of_id }
-          next unless quoted_status
+      owner_statuses.each do |owner_status|
+        quoted_status = quoted_statuses.detect { |status| status.id == owner_status.quote_of_id }
+        next unless quoted_status
 
-          begin
-            Quote.create!(
-              status_id: owner_status.id,
-              quoted_status_id: owner_status.quote_of_id,
-              state: 1,
-              account_id: owner_status.account_id,
-              quoted_account_id: quoted_status.account_id
-            )
-          rescue
-            # duplicate of snowflake
-          end
+        begin
+          Quote.create!(
+            status_id: owner_status.id,
+            quoted_status_id: owner_status.quote_of_id,
+            state: 1,
+            account_id: owner_status.account_id,
+            quoted_account_id: quoted_status.account_id
+          )
+        rescue
+          # duplicate of snowflake
         end
       end
     end
