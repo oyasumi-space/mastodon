26f5178a50a503c316632da83768c377427880df
KMY（雪あすか） <tt@kmycode.net>
2025-06-21 10:07:55 +0900

    Update 20250507033006_migrate_kmyblue_quotes.rb

--- db/migrate/20250507033006_migrate_kmyblue_quotes.rb
+++ db/migrate/20250507033006_migrate_kmyblue_quotes.rb
@@ -8,8 +8,10 @@ class MigrateKmyblueQuotes < ActiveRecord::Migration[8.0]
     ActiveRecord::Base.transaction do
       Status.where.not(quote_of_id: nil).select(:id, :quote_of_id, :account_id).in_batches do |owner_statuses|
         quoted_statuses = Status.where(id: owner_statuses.pluck(:quote_of_id)).select(:id, :account_id)
+        existing_quote_status_ids = Quote.where(status_id: owner_statuses.pluck(:id)).select(:id).pluck(:id)
 
         owner_statuses.each do |owner_status|
+          next if existing_quote_status_ids.include?(owner_status.id)
           quoted_status = quoted_statuses.detect { |status| status.id == owner_status.quote_of_id }
           next unless quoted_status
 
