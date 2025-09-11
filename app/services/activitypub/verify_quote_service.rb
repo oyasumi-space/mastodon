77f1f76edf6e39d77ce140e1d8d2b6e3f10877dd
KMY (Yuki Asuka) <tt@kmycode.net>
2025-09-11 23:49:31 +0000

    Fix test

--- app/services/activitypub/verify_quote_service.rb
+++ app/services/activitypub/verify_quote_service.rb
@@ -14,7 +14,7 @@ class ActivityPub::VerifyQuoteService < BaseService
 
     fetch_quoted_post_if_needed!(fetchable_quoted_uri, prefetched_body: prefetched_quoted_object)
 
-    return quote.accept! if Setting.auto_accept_legacy_quotes && legacy_quote_available? && (quote.legacy || quote.approval_uri == 'http://kmy.blue/ns#LegacyQuote')
+    return quote.accept! if Setting.auto_accept_legacy_quotes && (quote.legacy || (legacy_quote_available? && quote.approval_uri == 'http://kmy.blue/ns#LegacyQuote'))
 
     return if quote.approval_uri == 'http://kmy.blue/ns#LegacyQuote'
     return if quote.quoted_account&.local?
